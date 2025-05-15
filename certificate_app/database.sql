4658679edbc3ca3ae272394fe9f13957d99bc75a
prema0207 <premau2006@gmail.com>
2025-05-15 23:05:07 +0530

    Create database.sql

---
+++ certificate_app/database.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE certificates_db;
+USE certificates_db;
+
+CREATE TABLE certificates (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    certificate_type VARCHAR(50),
+    income DECIMAL(10,2),
+    community VARCHAR(100),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
